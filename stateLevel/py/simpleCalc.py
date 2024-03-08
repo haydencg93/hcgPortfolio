d4b48f37a1ba05561ae465c1dfe24dd84a7147d3
haydencg93 <74795625+haydencg93@users.noreply.github.com>
2024-03-07 22:43:59 -0600

    Create simpleCalc.py

---
+++ stateLevel/py/simpleCalc.py
@@ -0,0 +1,31 @@
+def simpleCalculator():
+    # numInput1 = input()
+    # operatorInput = input()
+    # numInput2 = input()
+    # if operatorInput == "+":
+    #     print(float(numInput1) + float(numInput2))
+    # elif operatorInput == "-":
+    #     print(float(numInput1) - float(numInput2))
+    # elif operatorInput == "*":
+    #     print(float(numInput1) * float(numInput2))
+    # elif operatorInput == "/":
+    #     print(float(numInput1) / float(numInput2))
+
+    ##################################################################
+    
+    # eqInput = input()
+    # for i in range(len(eqInput)):
+    #     print(eqInput[i])
+    #     if eqInput[i] == '+':
+    #         print(float(eqInput[:i]) + float(eqInput[i+1]))
+    #     elif eqInput[i] == '-':
+    #         print(float(eqInput[:i]) - float(eqInput[i+1]))
+    #     elif eqInput[i] == '*':
+    #         print(float(eqInput[:i]) * float(eqInput[i+1]))
+    #     elif eqInput[i] == '/':
+    #         print(float(eqInput[:i]) / float(eqInput[i+1]))
+    #     else:
+    #         continue
+
+if __name__ == '__main__':
+    simpleCalculator()
